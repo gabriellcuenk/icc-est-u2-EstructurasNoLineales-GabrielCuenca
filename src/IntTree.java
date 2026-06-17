aebb50dfbd60a2713039dbc89f1502683ffad0e3
Gabriel Cuenca <tuemgcuenca119@gmail.com>
2026-06-17 11:18:05 -0500

    Primer commit

---
+++ src/IntTree.java
@@ -0,0 +1,82 @@
+// Clase que representa un árbol binario de enteros
+public class IntTree {
+
+    private Node<Integer> root;
+
+    /// Constructor SIEMPRE inicializa LAS VARIABLES (ROOT)
+    public IntTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public Node<Integer> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<Integer> node) {
+        root = node;
+    }
+
+    public void setRoot(Integer value) {
+        Node<Integer> node = new Node<Integer>(value);
+        root = node;
+    }
+
+    public void insert(Integer value) { // 10
+        Node<Integer> node = new Node<Integer>(value);
+        root = insertRecursivo(root, node);
+    }
+
+    public void insert(Node<Integer> value) { // 10
+        root = insertRecursivo(root, value);
+    }
+
+    // recursivo para insertar valores ARBOL BINARIO
+    private Node<Integer> insertRecursivo(Node<Integer> actual, Node<Integer> nodeInsertar) {
+        if (actual == null) {
+            return nodeInsertar;
+        }
+
+        // validar si es mayoy o nenor y decidir si lo ingerso a la der o izq
+        if (actual.getValue() > nodeInsertar.getValue()) {
+            actual.setLeft(insertRecursivo(actual.getLeft(), nodeInsertar));
+        } else {
+            actual.setRight(insertRecursivo(actual.getRight(), nodeInsertar));
+        }
+
+        return actual;
+    }
+
+    public void preOrder() {
+        preOrderRecursivo(root);
+    }
+
+    private void preOrderRecursivo(Node<Integer> actual) {
+        if (actual == null)
+            return;
+        System.out.print(actual + " ");
+        preOrderRecursivo(actual.getLeft());
+        preOrderRecursivo(actual.getRight());
+    }
+
+    public void posOrder() {
+        posOrderRecursivo(root);
+    }
+
+    private void posOrderRecursivo(Node<Integer> actual) {
+        if (actual == null)
+            return;
+        posOrderRecursivo(actual.getLeft());
+        posOrderRecursivo(actual.getRight());
+        System.out.print(actual + " ");
+
+    }
+
+
+    // inorder
+    // niveles
+    // altura    
+}
